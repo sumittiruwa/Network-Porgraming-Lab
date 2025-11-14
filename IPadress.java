10bb364235be975db47a1f22e039d29a1d2ec1ac
khushimainali <sumittiruwa913@gmail.com>
2025-11-14 10:34:38 +0545

    Add program to display system IP address

--- IPadress.java
+++ IPadress.java
@@ -1,11 +1,20 @@
-import java.net.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class IPadress {
-public static void main(String[] args) {
+    public static void main(String[] args) {
         try {
-            InetAddress ip = InetAddress.getLocalHost();
-            System.out.println("IPv4 Address: " + ip.getHostAddress());
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+            // Get the local host (your system)
+            InetAddress localHost = InetAddress.getLocalHost();
+            
+            // Get the host IP address
+            String ipAddress = localHost.getHostAddress();
+            
+            // Display the IP address
+            System.out.println("Your system's IP address is: " + ipAddress);
+        } catch (UnknownHostException e) {
+            System.out.println("Unable to find IP address.");
+            e.printStackTrace();
         }
     }
 }
