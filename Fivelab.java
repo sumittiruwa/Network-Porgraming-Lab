b90e21134538beba66ddf4e1c52dba93f620b800
khushimainali <sumittiruwa913@gmail.com>
2025-11-15 08:17:45 +0545

    Add a program to list the interfaces in my system

---
+++ Fivelab.java
@@ -0,0 +1,21 @@
+import java.net.*;
+import java.util.*;
+
+public class Fivelab {
+    public static void main(String[] args) {
+        try {
+            Enumeration<NetworkInterface> interfaces =
+                    NetworkInterface.getNetworkInterfaces();
+
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface ni = interfaces.nextElement();
+
+                System.out.println("Interface Name: " + ni.getName());
+                System.out.println("Display Name : " + ni.getDisplayName());
+                System.out.println("----------------------------------------");
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
+}
